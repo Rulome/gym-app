9200502722877f86762f48430d61ffb3436b00de
Rubén Lozano Merino <rulome105@gmail.com>
2024-10-06 21:11:45 +0200

    Eliminamos imports en desuso

--- src/main/java/com/app/gym/service/implemetation/RoutineServiceImpl.java
+++ src/main/java/com/app/gym/service/implemetation/RoutineServiceImpl.java
@@ -1,11 +1,12 @@
 package com.app.gym.service.implemetation;
 
 import java.time.LocalDate;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 import com.app.gym.exceptions.CustomGymException;
+import com.app.gym.repository.dao.ClientRoutineRepository;
+import com.app.gym.repository.model.ClientRoutinePK;
 import com.app.gym.utils.Constants;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,7 +16,6 @@ import com.app.gym.repository.dao.ClientRepository;
 import com.app.gym.repository.dao.RoutineRepository;
 import com.app.gym.repository.model.Client;
 import com.app.gym.repository.model.ClientRoutine;
-import com.app.gym.repository.model.ClientRoutinePK;
 import com.app.gym.repository.model.Routine;
 import com.app.gym.service.RoutineService;
 import com.app.gym.utils.Assembler;
@@ -32,15 +32,21 @@ public class RoutineServiceImpl implements RoutineService {
 	/** The client repository. */
 	private final ClientRepository clientRepository;
 
+	/** The client routine repository. */
+	private final ClientRoutineRepository clientRoutineRepository;
+
 	/**
-	 * Instantiates a new routine service impl.
+	 * Instantiates a new Routine service.
 	 *
-	 * @param routineRepository the routine repository
-	 * @param clientRepository  the client repository
+	 * @param routineRepository       the routine repository
+	 * @param clientRepository        the client repository
+	 * @param clientRoutineRepository the client routine repository
 	 */
-	public RoutineServiceImpl(RoutineRepository routineRepository, ClientRepository clientRepository) {
+	public RoutineServiceImpl(RoutineRepository routineRepository, ClientRepository clientRepository,
+							  ClientRoutineRepository clientRoutineRepository) {
 		this.routineRepository = routineRepository;
 		this.clientRepository = clientRepository;
+		this.clientRoutineRepository = clientRoutineRepository;
 	}
 
 	@Override
@@ -54,24 +60,46 @@ public class RoutineServiceImpl implements RoutineService {
 	@Transactional
 	public void asign(Long routineID, String clientID) {
 
-		Optional<Client> client = clientRepository.findById(clientID);
-		Optional<Routine> routine = routineRepository.findById(routineID);
+		Optional<Client> clientOpt = clientRepository.findById(clientID);
+		Optional<Routine> routineOpt = routineRepository.findById(routineID);
 
-		if (client.isEmpty() && routine.isEmpty()){
+		// Verificaciones iniciales de existencia de cliente y rutina
+		if (clientOpt.isEmpty() && routineOpt.isEmpty()){
 			throw new CustomGymException(Constants.CODE_001, Constants.CODE_001_DESCRIPTION);
-		} else if (client.isEmpty()) {
-			throw new CustomGymException(Constants.CODE_002, Constants.CODE_002_DESCRIPTION);
-		} else if (routine.isEmpty()) {
-			throw new CustomGymException(Constants.CODE_003, Constants.CODE_003_DESCRIPTION);
-		} else {
-			Routine r = routine.get();
-			ClientRoutinePK crPK = new ClientRoutinePK(clientID, routineID);
-			ClientRoutine cr = new ClientRoutine(crPK, LocalDate.now(), null, r, client.get());
-			Set<ClientRoutine> crSet = new HashSet<>(r.getClientRoutines());
-			crSet.add(cr);
-			r.setClients(crSet);
-			routineRepository.save(r);
 		}
+		if (clientOpt.isEmpty()) {
+			throw new CustomGymException(Constants.CODE_002, Constants.CODE_002_DESCRIPTION);
+		}
+		if (routineOpt.isEmpty()) {
+			throw new CustomGymException(Constants.CODE_003, Constants.CODE_003_DESCRIPTION);
+		}
+
+		Client client = clientOpt.get();
+		Routine routine = routineOpt.get();
+
+		// Verificar si el cliente ya tiene una rutina activa
+		List<ClientRoutine> activeRoutines = clientRoutineRepository.findByClientIdAndEndIsNull(clientID);
+
+		if (!activeRoutines.isEmpty()) {
+			throw new CustomGymException(Constants.CODE_004, Constants.CODE_004_DESCRIPTION);
+		}
+
+		// Verificar si ya existe la relación entre el cliente y la rutina
+		ClientRoutinePK crPK = new ClientRoutinePK( LocalDate.now(), clientID, routineID);
+		Optional<ClientRoutine> existingCrOpt = clientRoutineRepository.findById(crPK);
+
+		if (existingCrOpt.isPresent()) {
+			throw new CustomGymException(Constants.CODE_005, Constants.CODE_005_DESCRIPTION);
+		}
+
+		// Crear una nueva relación entre Cliente y Rutina
+		ClientRoutine newCr = new ClientRoutine(crPK, null, routine, client);
+
+		// Añadir la relación a las colecciones adecuadas
+		routine.getClientRoutines().add(newCr);
+
+		// Guardar la nueva relación en la base de datos
+		clientRoutineRepository.save(newCr);
 	}
 
 }
